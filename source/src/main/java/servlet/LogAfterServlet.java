92e15a2f35698efba5e611e122a6c2be6cd3cfd5
plusdojo2025-suzukihikaru <suzuki-hikaru-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-18 11:19:31 +0900

    logServlet

---
+++ source/src/main/java/servlet/LogAfterServlet.java
@@ -0,0 +1,64 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogAfterServlet
+ */
+@WebServlet("/LogAfterServlet")
+public class LogAfterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogAfterServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// ログインしていない場合
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/B3/LoginServlet");
+			return;
+		}
+				
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/logafter.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// データの入力後にもしもログインしていない場合ログインサーブレットにリダイレクト
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/B3/LoginServlet");
+			return;
+		}
+		
+		// リクエストパラメータを取得
+		request.setCharacterEncoding("UTF-8");
+		String perfume_id_s = request.getParameter("perfume_id");
+		int perfume_id = Integer.parseInt(perfume_id_s);
+		
+		String middle_note = request.getParameter("middle_note");
+		String last_note = request.getParameter("last_note");
+	}
+
+}
---
+++ source/src/main/webapp/WEB-INF/jsp/logafter.jsp
@@ -0,0 +1,47 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Parfüm Note | Log</title>
+</head>
+<body>
+	<header>
+		<jsp:include page="/WEB-INF/jsp/header.jsp" />
+	</header>
+	
+	<label for="perfume_id">*商品名</label>
+	<select name="perfume_id">
+	<!-- perfume_nameかidかわからない -->
+		<option value="1">香水A</option>
+		<option value="2">香水B</option>
+		<option value="3">香水C</option>
+	</select>
+	
+	<p>*</p>
+	<img src="わからない" alt="香水画像">
+	<!-- 商品名に基づいた画像を表示させる方法が分からない -->
+	
+	<div>登録済みのタグ
+		<!-- イメージタグを表示させる方法が分からない -->
+	</div>
+	
+	<p>・使用後登録</p>
+	<hr>
+	
+	<label for="middle_note">ミドルノート</label>
+	<textarea name="middle_note"></textarea><br>
+	
+	<label for="last_note">ラストノート</label>
+	<textarea name="last_note"></textarea><br>
+	
+	<label for="thoughts">感想</label>
+	<textarea name="thoughts"></textarea>
+	
+	<p>記録すると自動的にカレンダーに戻ります</p>
+	
+	<input type="submit" value="記録">
+	<input type="submit" value="使用前へ">
+</body>
+</html>
