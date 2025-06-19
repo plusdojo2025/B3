63da9c615ac1696862c6d432ed5c9af9bd14faff
plusdojo2025-Sato <sato-rikuto-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-19 10:00:09 +0900

    Update Big_category.java

--- source/src/main/java/dto/Big_category.java
+++ source/src/main/java/dto/Big_category.java
@@ -1,19 +1,15 @@
 package dto;
 
 import java.io.Serializable;
-import java.sql.Timestamp;
 
 public class Big_category implements Serializable {
 
     private int id;              
     private String sweet;           
     private String fresh;           
-    private String spicyt;          
-    
+    private String spicyt;             
 	private String relax;           
-    private Timestamp created_at;   
-    private Timestamp updated_at;
-    
+
 	public int getId() {
 		return id;
 	}
@@ -44,28 +40,15 @@ public class Big_category implements Serializable {
 	public void setRelax(String relax) {
 		this.relax = relax;
 	}
-	public Timestamp getCreated_at() {
-		return created_at;
-	}
-	public void setCreated_at(Timestamp created_at) {
-		this.created_at = created_at;
-	}
-	public Timestamp getUpdated_at() {
-		return updated_at;
-	}
-	public void setUpdated_at(Timestamp updated_at) {
-		this.updated_at = updated_at;
-	} 
 	
-	public Big_category(int id, String sweet, String fresh, String spicyt, String relax, Timestamp created_at,
-			Timestamp updated_at) {
+	
+	public Big_category(int id, String sweet, String fresh, String spicyt, String relax) {
 		super();
 		this.id = id;
 		this.sweet = sweet;
 		this.fresh = fresh;
 		this.spicyt = spicyt;
 		this.relax = relax;
-		this.created_at = created_at;
-		this.updated_at = updated_at;
+		
 	}
 }
