3c4091226b98bf7c136f0076a9bbc79e5ae8dccd
plusdojo2025-katok <kato-kana-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-20 15:23:24 +0900

    0620 15:23

--- source/src/main/java/dao/Perfume_imagesDAO.java
+++ source/src/main/java/dao/Perfume_imagesDAO.java
@@ -5,111 +5,67 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-import dto.Bc;
-import dto.Perfumes;
+import dto.Perfume_images;
 
 public class Perfume_imagesDAO {
 	// 引数card指定された項目で検索して、取得されたデータのリストを返す
-		public boolean select(Perfumes pfi) {
+		public List<Perfume_images> select(Perfume_images pfi) {
 			Connection conn = null;
-			boolean result = false;
+			List<Perfume_images> imgList = new ArrayList<>();
 			try {
 				// JDBCドライバを読み込む
 				Class.forName("com.mysql.cj.jdbc.Driver");
 				// データベースに接続する
-				conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/webapp2?"
+				conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/b3?"
 						+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
 						"root", "password");
 
 				// SQL文を準備する
-				String sql = "SELECT number, company, department, position, name, "
-						+ "zipcode, address, phone, fax, email, remarks "
-						+ "FROM Bc "
-						+ "WHERE company LIKE ? AND department LIKE ? AND position LIKE ? "
-						+ "AND name LIKE ? AND zipcode LIKE ? AND address LIKE ? AND phone LIKE ? "
-						+ "AND fax LIKE ? AND email LIKE ? AND remarks LIKE ? "
-						+ "ORDER BY number";
+				String sql = "SELECT perfume_id, big_id, small_id"
+						+ "FROM perfume_images"
+						+ "WHERE perfume_id LIKE ? AND big_id LIKE ? AND small_id LIKE ?";
 				PreparedStatement pStmt = conn.prepareStatement(sql);
 
 				// SQL文を完成させる
-				if (card.getCompany() != null) {
-					pStmt.setString(1, "%" + card.getCompany() + "%");
+				
+				if (pfi.getPerfume_id() != 0) {
+					pStmt.setInt(1, pfi.getPerfume_id());
 				} else {
-					pStmt.setString(1, "%");
+					pStmt.setNull(1,java.sql.Types.INTEGER );
 				}
-				if (card.getDepartment() != null) {
-					pStmt.setString(2, "%" + card.getDepartment() + "%");
+				if (pfi.getBig_id() != 0) {
+					pStmt.setInt(2, pfi.getBig_id());
 				} else {
-					pStmt.setString(2, "%");
+					pStmt.setNull(2,java.sql.Types.INTEGER );
 				}
-				if (card.getPosition() != null) {
-					pStmt.setString(3, "%" + card.getPosition() + "%");
+				if (pfi.getSmall_id() != 0) {
+					pStmt.setInt(3, pfi.getSmall_id());
 				} else {
-					pStmt.setString(3, "%");
+					pStmt.setNull(3,java.sql.Types.INTEGER );
 				}
-				if (card.getName() != null) {
-					pStmt.setString(4, "%" + card.getName() + "%");
-				} else {
-					pStmt.setString(4, "%");
-				}
-				if (card.getZipcode() != null) {
-					pStmt.setString(5, "%" + card.getZipcode() + "%");
-				} else {
-					pStmt.setString(5, "%");
-				}
-				if (card.getAddress() != null) {
-					pStmt.setString(6, "%" + card.getAddress() + "%");
-				} else {
-					pStmt.setString(6, "%");
-				}
-				if (card.getPhone() != null) {
-					pStmt.setString(7, "%" + card.getPhone() + "%");
-				} else {
-					pStmt.setString(7, "%");
-				}
-				if (card.getFax() != null) {
-					pStmt.setString(8, "%" + card.getFax() + "%");
-				} else {
-					pStmt.setString(8, "%");
-				}
-				if (card.getEmail() != null) {
-					pStmt.setString(9, "%" + card.getEmail() + "%");
-				} else {
-					pStmt.setString(9, "%");
-				}
-				if (card.getRemarks() != null) {
-					pStmt.setString(10, "%" + card.getRemarks() + "%");
-				} else {
-					pStmt.setString(10, "%");
-				}
+				
 
 				// SQL文を実行し、結果表を取得する
 				ResultSet rs = pStmt.executeQuery();
 
 				// 結果表をコレクションにコピーする
 				while (rs.next()) {
-					Bc bc = new Bc(
-							rs.getInt("number"), 
-							rs.getString("company"),
-							rs.getString("department"),
-							rs.getString("position"),
-							rs.getString("name"),
-							rs.getString("zipcode"),
-							rs.getString("address"),
-							rs.getString("phone"),
-							rs.getString("fax"),
-							rs.getString("email"),
-							rs.getString("remarks")
+					Perfume_images image = new Perfume_images(
+							rs.getInt("perfume_id"),
+							rs.getInt("big_id"),
+							rs.getInt("small_id")
 							);
-					cardList.add(bc);
+					imgList.add(image);
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
-				cardList = null;
+				imgList = null;
 			} catch (ClassNotFoundException e) {
 				e.printStackTrace();
-				cardList = null;
+				imgList = null;
 			} finally {
 				// データベースを切断
 				if (conn != null) {
@@ -117,17 +73,17 @@ public class Perfume_imagesDAO {
 						conn.close();
 					} catch (SQLException e) {
 						e.printStackTrace();
-						cardList = null;
+						imgList = null;
 					}
 				}
 			}
 
 			// 結果を返す
-			return cardList;
+			return imgList;
 		}
 
 	// 引数cardで指定されたレコードを登録し、成功したらtrueを返す
-		public boolean insert(Perfumes pfi) {
+		public boolean insert(Perfume_images pfi) {
 			Connection conn = null;
 			boolean result = false;
 
@@ -136,29 +92,29 @@ public class Perfume_imagesDAO {
 				Class.forName("com.mysql.cj.jdbc.Driver");
 
 				// データベースに接続する
-				conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/b3"
+				conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/b3?"
 						+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
 						"root", "password");
 
 				// SQL文を準備する
-				String sql = "INSERT INTO Perfumes VALUES (?, ?, ?)";
+				String sql = "INSERT INTO Perfume_images VALUES (?, ?, ?)";
 				PreparedStatement pStmt = conn.prepareStatement(sql);
 
 				// SQL文を完成させる
-				if (pfi.getGenre() != null) {
-					pStmt.setString(1, pfi.getGenre());
+				if (pfi.getPerfume_id() != 0) {
+					pStmt.setInt(1, pfi.getPerfume_id());
 				} else {
-					pStmt.setString(1, "");
+					pStmt.setInt(1, 0);
 				}
-				if (pfi.getBig_id() != null) {
-					pStmt.setString(2, pfi.getBig_id());
+				if (pfi.getBig_id() != 0) {
+					pStmt.setInt(2, pfi.getBig_id());
 				} else {
-					pStmt.setString(2, "");
+					pStmt.setInt(2, 0);
 				}
-				if (pfi.getDetail() != null) {
-					pStmt.setString(3, pfi.getDetail());
+				if (pfi.getSmall_id() != 0) {
+					pStmt.setInt(3, pfi.getSmall_id());
 				} else {
-					pStmt.setString(3, "");
+					pStmt.setInt(3, 0);
 				}
 				
 
@@ -185,10 +141,12 @@ public class Perfume_imagesDAO {
 			return result;
 		}
 	
-	// 引数card指定された項目で検索して、取得されたデータのリストを返す
-	public boolean insert(Perfumes pfi) {
+	
+	// 引数cardで指定されたレコードを更新し、成功したらtrueを返す
+	public boolean update(Perfume_images pfi) {
 		Connection conn = null;
 		boolean result = false;
+
 		try {
 			// JDBCドライバを読み込む
 			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -199,183 +157,28 @@ public class Perfume_imagesDAO {
 					"root", "password");
 
 			// SQL文を準備する
-			String sql = "SELECT number, company, department, position, name, "
-					+ "zipcode, address, phone, fax, email, remarks "
-					+ "FROM Bc "
-					+ "WHERE company LIKE ? AND department LIKE ? AND position LIKE ? "
-					+ "AND name LIKE ? AND zipcode LIKE ? AND address LIKE ? AND phone LIKE ? "
-					+ "AND fax LIKE ? AND email LIKE ? AND remarks LIKE ? "
-					+ "ORDER BY number";
+			String sql = "UPDATE Perfume_images SET perfume_id=?, big_id=?, small_id=?"
+					+ "WHERE Perfume_id=?";
+			
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			// SQL文を完成させる
-			if (card.getCompany() != null) {
-				pStmt.setString(1, "%" + card.getCompany() + "%");
+			if (pfi.getPerfume_id() != 0) {
+				pStmt.setInt(1, pfi.getPerfume_id());
 			} else {
-				pStmt.setString(1, "%");
+				pStmt.setInt(1, 0);
 			}
-			if (card.getDepartment() != null) {
-				pStmt.setString(2, "%" + card.getDepartment() + "%");
+			if (pfi.getBig_id() != 0) {
+				pStmt.setInt(2, pfi.getBig_id());
 			} else {
-				pStmt.setString(2, "%");
+				pStmt.setInt(2, 0);
 			}
-			if (card.getPosition() != null) {
-				pStmt.setString(3, "%" + card.getPosition() + "%");
+			if (pfi.getSmall_id() != 0) {
+				pStmt.setInt(3, pfi.getSmall_id());
 			} else {
-				pStmt.setString(3, "%");
+				pStmt.setInt(3, 0);
 			}
-			if (card.getName() != null) {
-				pStmt.setString(4, "%" + card.getName() + "%");
-			} else {
-				pStmt.setString(4, "%");
-			}
-			if (card.getZipcode() != null) {
-				pStmt.setString(5, "%" + card.getZipcode() + "%");
-			} else {
-				pStmt.setString(5, "%");
-			}
-			if (card.getAddress() != null) {
-				pStmt.setString(6, "%" + card.getAddress() + "%");
-			} else {
-				pStmt.setString(6, "%");
-			}
-			if (card.getPhone() != null) {
-				pStmt.setString(7, "%" + card.getPhone() + "%");
-			} else {
-				pStmt.setString(7, "%");
-			}
-			if (card.getFax() != null) {
-				pStmt.setString(8, "%" + card.getFax() + "%");
-			} else {
-				pStmt.setString(8, "%");
-			}
-			if (card.getEmail() != null) {
-				pStmt.setString(9, "%" + card.getEmail() + "%");
-			} else {
-				pStmt.setString(9, "%");
-			}
-			if (card.getRemarks() != null) {
-				pStmt.setString(10, "%" + card.getRemarks() + "%");
-			} else {
-				pStmt.setString(10, "%");
-			}
-
-			// SQL文を実行し、結果表を取得する
-			ResultSet rs = pStmt.executeQuery();
-
-			// 結果表をコレクションにコピーする
-			while (rs.next()) {
-				Bc bc = new Bc(
-						rs.getInt("number"), 
-						rs.getString("company"),
-						rs.getString("department"),
-						rs.getString("position"),
-						rs.getString("name"),
-						rs.getString("zipcode"),
-						rs.getString("address"),
-						rs.getString("phone"),
-						rs.getString("fax"),
-						rs.getString("email"),
-						rs.getString("remarks")
-						);
-				cardList.add(bc);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			cardList = null;
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			cardList = null;
-		} finally {
-			// データベースを切断
-			if (conn != null) {
-				try {
-					conn.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-					cardList = null;
-				}
-			}
-		}
-
-		// 結果を返す
-		return cardList;
-	}
-
-	
-
-	// 引数cardで指定されたレコードを更新し、成功したらtrueを返す
-	public boolean update(Bc card) {
-		Connection conn = null;
-		boolean result = false;
-
-		try {
-			// JDBCドライバを読み込む
-			Class.forName("com.mysql.cj.jdbc.Driver");
-
-			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/webapp2?"
-					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
-					"root", "password");
-
-			// SQL文を準備する
-			String sql = "UPDATE Bc SET company=?, department=?, position=?, "
-					+ "name=?, zipcode=?, address=?, phone=?, fax=?, email=?, remarks=? "
-					+ "WHERE number=?";
-			PreparedStatement pStmt = conn.prepareStatement(sql);
-
-			// SQL文を完成させる
-			if (card.getCompany() != null) {
-				pStmt.setString(1, card.getCompany());
-			} else {
-				pStmt.setString(1, "");
-			}
-			if (card.getDepartment() != null) {
-				pStmt.setString(2, card.getDepartment());
-			} else {
-				pStmt.setString(2, "");
-			}
-			if (card.getPosition() != null) {
-				pStmt.setString(3, card.getPosition());
-			} else {
-				pStmt.setString(3, "");
-			}
-			if (card.getName() != null) {
-				pStmt.setString(4, card.getName());
-			} else {
-				pStmt.setString(4, "");
-			}
-			if (card.getZipcode() != null) {
-				pStmt.setString(5, card.getZipcode());
-			} else {
-				pStmt.setString(5, "");
-			}
-			if (card.getAddress() != null) {
-				pStmt.setString(6, card.getAddress());
-			} else {
-				pStmt.setString(6, "");
-			}
-			if (card.getPhone() != null) {
-				pStmt.setString(7, card.getPhone());
-			} else {
-				pStmt.setString(7, "");
-			}
-			if (card.getFax() != null) {
-				pStmt.setString(8, card.getFax());
-			} else {
-				pStmt.setString(8, "");
-			}
-			if (card.getEmail() != null) {
-				pStmt.setString(9, card.getEmail());
-			} else {
-				pStmt.setString(9, "");
-			}
-			if (card.getRemarks() != null) {
-				pStmt.setString(10, card.getRemarks());
-			} else {
-				pStmt.setString(10, "");
-			}
-			pStmt.setInt(11, card.getNumber());
+			pStmt.setInt(3, pfi.getPerfume_id());
 
 			// SQL文を実行する
 			if (pStmt.executeUpdate() == 1) {
@@ -401,7 +204,7 @@ public class Perfume_imagesDAO {
 	}
 
 	// 引数cardで指定された番号のレコードを削除し、成功したらtrueを返す
-	public boolean delete(Bc card) {
+	public boolean delete(Perfume_images pfi) {
 		Connection conn = null;
 		boolean result = false;
 
@@ -410,16 +213,16 @@ public class Perfume_imagesDAO {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 
 			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/webapp2?"
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/b3?"
 					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
 					"root", "password");
 
 			// SQL文を準備する
-			String sql = "DELETE FROM Bc WHERE number=?";
+			String sql = "DELETE FROM Perfume_images WHERE Perfume_id=?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			// SQL文を完成させる
-			pStmt.setInt(1, card.getNumber());
+			pStmt.setInt(1, pfi.getPerfume_id());
 
 			// SQL文を実行する
 			if (pStmt.executeUpdate() == 1) {
